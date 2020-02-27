75477a07c564bdce9b245f6b70478c4e1e06e81a
Dakota Logan <xeliusone@gmail.com>
2020-02-26 20:53:02 -0700

    server.js env setup properly *facedesk* uwu

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const
 fs = require ("fs"),
 http = require ("http");
+require("dotenv").config();
 
-const app = http.createServer((req, res) => {
+http.createServer((req, res) => {
 	fs.readFile(__dirname + "/index.html", (err, data) => {
 		res.writeHead(200);
 		res.end(data);
 	});
 }).listen(process.env.PORT);
+console.log(/*"running on port "+*/process.env.PORT);
