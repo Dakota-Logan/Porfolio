5071349ffad9c085c89cd8eee1ced42de783a47e
Dakota Logan <xeliusone@gmail.com>
2020-02-26 20:25:20 -0700

    reset

--- server/routes/blogs.js
+++
@@ -1,8 +0,0 @@
-exports.get = (req, res) => {
-	res.send(JSON.stringify("Hello!"))
-}
-
-exports.getById = (req, res) => {
-	let data = {data: "hello: " + req.params.id}
-	res.send(JSON.stringify(data));
-}
